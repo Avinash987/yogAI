f2e21edcad5a8d6e7cd71671c33fe911845dda9f
Lalith Avinash <user@example.com>
2020-06-20 17:01:52 +0530

    additional bugs fixed

--- lib/login.dart
+++ lib/login.dart
@@ -2,9 +2,9 @@ import 'package:camera/camera.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:yoga_guru/home.dart';
-import 'package:yoga_guru/register.dart';
-import 'package:yoga_guru/util/auth.dart';
+import 'package:yogAI/home.dart';
+import 'package:yogAI/register.dart';
+import 'package:yogAI/util/auth.dart';
 
 class Login extends StatefulWidget {
   final List<CameraDescription> cameras;
